4f5389fbb60bbacc9baa27bc32ac0b1eff03da2a
Jason Gross <jgross@mit.edu>
2017-11-14 19:28:36 -0500

    Remove cache manifest
    Apparently it's going out of style

--- update-version.sh
+++ update-version.sh
@@ -5,11 +5,5 @@ echo VERSION:
 echo -n $("$BASE/version.sh") | tee "$BASE/VERSION"
 echo
 echo
-echo cache.manifest:
-sed s"/^# version.*$/# version $("$BASE/version.sh")/i" "$BASE/cache.manifest.in" > "$BASE/cache.manifest"
-echo >> "$BASE/cache.manifest"
-echo "# Time Stamp: `date +%s`" >> "$BASE/cache.manifest"
-cat "$BASE/cache.manifest"
-chmod -w "$BASE/VERSION" "$BASE/cache.manifest"
 ## create google analytics file if it doesn't exist
 # touch GOOGLE-SITE-VERIFICATION
